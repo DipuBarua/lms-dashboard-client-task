13d96b993d2401bd06176d6db06503fa96b2e50d
DipuBarua <dipubarua1997@gmail.com>
2024-08-14 22:00:29 +0600

    lms dashboard using react,tailwind

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1 className="text-3xl font-bold underline">
+        This is my job task: LMS dashboar
+      </h1>
+
+    </>
+  )
+}
+
+export default App
